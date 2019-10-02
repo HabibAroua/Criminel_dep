a2c37fe81b457059d7ff7ee23931092da12485a0
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-10-02 17:48:43 +0100

    edit table

--- db/migrate/20191002162649_create_proofs.rb
+++ db/migrate/20191002162649_create_proofs.rb
@@ -4,7 +4,7 @@ class CreateProofs < ActiveRecord::Migration[5.1]
     create_table :proofs do |t|
       t.string :type_proof , :limit=>25
       t.text :content
-
+      #date of add and edit
       t.timestamps
     end
   end
